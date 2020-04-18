556b9fe4e26921a2042ccee6255214d432e7c8dc
tiagob85 <tiago.gpes@gmail.com>
2020-04-18 18:58:04 -0300

    Aplicativo Github Semana Omnistack 10 - Backend e Mobile completo.

---
+++ mobile/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <>    
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routes/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  teste:{
+    fontSize: 30,
+    backgroundColor: '#00F',
+    color: '#0F0'
+  }
+});
